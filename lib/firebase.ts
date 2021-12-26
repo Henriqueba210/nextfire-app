dfc8f3440a5b8e63ec204bf5df815db1331bd4c3
Henrique Barros de Almeida <henrique.balmeida@outlook.com>
2021-12-26 11:44:15 -0300

    feat: added typescript and firebase

---
+++ lib/firebase.ts
@@ -0,0 +1,23 @@
+import { FirebaseOptions } from '@firebase/app'
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
+import 'firebase/compat/storage'
+
+const firebaseConfig: FirebaseOptions = {
+  apiKey: "AIzaSyBGSoAnm0RGK_Ub9AUuSMLeZRL3esBEUn8",
+  authDomain: "nextfire-c85f5.firebaseapp.com",
+  projectId: "nextfire-c85f5",
+  storageBucket: "nextfire-c85f5.appspot.com",
+  messagingSenderId: "507716848873",
+  appId: "1:507716848873:web:5b6579cc653e5b2b2b7478",
+  measurementId: "G-VMVCN37YFZ"
+}
+
+if(!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig)
+}
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+export const storage = firebase.storage()
